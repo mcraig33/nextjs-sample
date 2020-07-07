9ac4a7b2fd1e3e356a46751520159d119053cf34
Michael Craig <mcraig@rpa.com>
2020-07-07 11:42:00 -0400

    Add getInitialProps method to set server-side Props on components

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import User from '../../components/User'
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>The Aux Page</h1>
+        <h1>The Aux Page - {props.appName}</h1>
         <User name="Max" age={39} />
     </div>
 );
+
+authIndexPage.getInitialProps = context => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({appName: 'NextJS App Name(Auth)'});
+        }, 1000);
+    });
+    return promise;
+}
 
 export default authIndexPage;
